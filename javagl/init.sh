196be6d046418e2993c7e0097638cbcdd0a812fe
Riccardo Balbo <riccardo0blb@gmail.com>
2020-03-16 16:52:50 +0100

    Update init.sh

--- javagl/init.sh
+++ javagl/init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-pulseaudio --daemonize --system
-pulseaudio --check --system 
+pulseaudio --start  
 /sbin/start-stop-daemon --start --quiet --pidfile /tmp/xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- ${DISPLAY} -screen 0 ${DISPLAY_RESOLUTION} -ac +extension GLX +render
 $@
